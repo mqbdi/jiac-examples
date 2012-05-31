60abc0730d7f2cc9213b0e2f083e29c3c0a14f78
mqbdi <m.q.burkhardt@gmail.com>
2012-05-31 11:00:16 +0200

    adding some comments

--- service-parameter-example/src/main/java/de/btopia/examples/beans/InitiatorAgentBean.java
+++ service-parameter-example/src/main/java/de/btopia/examples/beans/InitiatorAgentBean.java
@@ -1,6 +1,5 @@
 package de.btopia.examples.beans;
 
-
 import java.io.Serializable;
 
 import de.dailab.jiactng.agentcore.AbstractAgentBean;
@@ -9,32 +8,46 @@ import de.dailab.jiactng.agentcore.action.ActionResult;
 import de.dailab.jiactng.agentcore.environment.ResultReceiver;
 import de.dailab.jiactng.agentcore.ontology.IActionDescription;
 
-public class InitiatorAgentBean extends AbstractAgentBean implements ResultReceiver {
-	
+public class InitiatorAgentBean extends AbstractAgentBean implements
+		ResultReceiver {
+
 	private int countdown = 11;
-	
+
 	@Override
 	public void execute() {
-		IActionDescription action = thisAgent.searchAction(new Action(ResponseAgentBean.ACTION_MULTIPLE_RESULTS));
+		/*
+		 * In this execute() method we search for one single action by using the
+		 * JIAC's template mechanism. We just use the service name and invoke
+		 * the service, if someone was found (not null).
+		 */
+		IActionDescription action = thisAgent.searchAction(new Action(
+				ResponseAgentBean.ACTION_MULTIPLE_RESULTS));
 		if (action != null) {
-			invoke(action, new Serializable[]{Integer.valueOf(--countdown)}, this);
+			invoke(action, new Serializable[] { Integer.valueOf(--countdown) },
+					this);
 		}
 	}
-	
 
 	@Override
 	public void receiveResult(ActionResult result) {
+		/*
+		 * The receiveResult() method is been called, if a service invocations
+		 * returns. An ActionResult contains the original invoked Action and the
+		 * result values of the executed service.
+		 */
 		String name = result.getAction().getName();
 		if (ResponseAgentBean.ACTION_MULTIPLE_RESULTS.equals(name)) {
 			Serializable[] res = result.getResults();
 			Serializable failure = result.getFailure();
 			if (failure != null) {
 				log.warn("service invocation returned failure: " + failure);
-			}
-			else {
+			} else {
 				Integer counter = (Integer) res[0];
-				Double sqrt = (Double) res[1];
-				log.info(ResponseAgentBean.ACTION_MULTIPLE_RESULTS + " returned two results: " + String.format("%d %2.3f", counter, sqrt));
+				Integer param = (Integer) res[1];
+				Double sqrt = (Double) res[2];
+				log.info(ResponseAgentBean.ACTION_MULTIPLE_RESULTS
+						+ " returned three results: "
+						+ String.format("%d %d %2.3f", counter, param, sqrt));
 			}
 		}
 	}
--- service-parameter-example/src/main/java/de/btopia/examples/beans/ResponseAgentBean.java
+++ service-parameter-example/src/main/java/de/btopia/examples/beans/ResponseAgentBean.java
@@ -1,6 +1,5 @@
 package de.btopia.examples.beans;
 
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +13,25 @@ import de.dailab.jiactng.agentcore.ontology.IActionDescription;
 
 public class ResponseAgentBean extends AbstractAgentBean implements IEffector {
 
+	/**
+	 * This action got one parameter and returns three result values.
+	 */
 	public static final String ACTION_MULTIPLE_RESULTS = "ResponseAgentBean.MultipleResults";
 
 	private int invocationCounter = 0;
 
 	private void doMultipleResults(DoAction doAction) {
+		/*
+		 * A service invocation (DoAction) contains an array of parameter. The
+		 * number and types of parameter should be identical to the published
+		 * Action.
+		 */
 		Integer arg0 = (Integer) doAction.getParams()[0];
 		if (arg0 > 0) {
 			Double sqrt = Math.sqrt(arg0.doubleValue());
 			returnResult(doAction,
-					new Serializable[] { 
-						Integer.valueOf(++invocationCounter),sqrt }
-			);
+					new Serializable[] { Integer.valueOf(++invocationCounter),
+							arg0, sqrt });
 		} else {
 			returnFailure(doAction, new String(
 					"could not compute sqrt of negative numbers."));
@@ -34,10 +40,19 @@ public class ResponseAgentBean extends AbstractAgentBean implements IEffector {
 
 	@Override
 	public List<? extends IActionDescription> getActions() {
+		/*
+		 * The getActions() method will be used to publish the services of this
+		 * agent bean. It returns a list of actions, i.e., a list of service
+		 * signatures. An action specifies: (1.) the service name (String) (2.)
+		 * the provider that should be called (IEffector) (3.) the parameter
+		 * types (Class[]) (4.)the result types (Class[]) and (5.) since
+		 * JIAC~5.1.0 the action scope that specifies the publishing range. This
+		 * method will be called within the usual agents life cycle.
+		 */
 		List<Action> ret = new ArrayList<Action>();
 		Action action = new Action(ACTION_MULTIPLE_RESULTS, this,
 				new Class[] { Integer.class }, new Class[] { Integer.class,
-						Double.class });
+						Integer.class, Double.class });
 		action.setScope(ActionScope.NODE);
 		ret.add(action);
 		return ret;
@@ -45,8 +60,20 @@ public class ResponseAgentBean extends AbstractAgentBean implements IEffector {
 
 	@Override
 	public void doAction(DoAction doAction) throws Exception {
+		/*
+		 * The doAction method will be called by the agents execution cycle on
+		 * DoAction events. Usually, a DoAction will be delivered to an agent,
+		 * if the agent publishes one or more services.
+		 * 
+		 * A DoAction contains the Action that was called, the parameter values
+		 * for this invocation and a description of the invoking agent, if the
+		 * agent is interested in the service results.
+		 */
 		String name = doAction.getAction().getName();
 		if (ACTION_MULTIPLE_RESULTS.equals(name)) {
+			/*
+			 * We just delegate the invocation to a special method.
+			 */
 			this.doMultipleResults(doAction);
 		} else {
 			log.error("this service invocation could not executed by me.");
